d9464f02cd5084157ca7a698bf2d9d264edfa461
Fernando Hoyos <fernandolhoyosh@gmail.com>
2024-12-02 18:03:00 -0500

    enlaces redes sociales

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,13 +3,13 @@ import "./Footer.css"
 const Footer = () => {
     return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
         <div className='redes'>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.linkedin.com/in/fernandolhoyosh/'>
                 <img src="/img/facebook.png" alt='Facebook' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://x.com/fernandolhoyosh'>
                 <img src="/img/twitter.png" alt='twitter' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.instagram.com/fernandolhoyosh/'>
                 <img src="/img/instagram.png" alt='instagram' />
             </a>
         </div>
